edd729445ecd6e6970f474c079771b777fd792b3
Jianan Wu <jianan.wu@anu.edu.au>
2021-03-22 17:06:27 +1100

    Sample Map upload

---
+++ DREAMS Unity/DREAMS/Assets/Scripts/DemoScripts/FPSCounter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour
+{
+    public int avgFrameRate;
+    public GameObject Text;
+    public Material skybox1, skybox2;
+
+    public void Update()
+    {
+        float current = 0;
+        current = (int)(1f / Time.unscaledDeltaTime);
+        avgFrameRate = (int)current;
+        Text.GetComponent<TextMesh>().text = avgFrameRate.ToString() + " FPS";
+        
+    }
+}
