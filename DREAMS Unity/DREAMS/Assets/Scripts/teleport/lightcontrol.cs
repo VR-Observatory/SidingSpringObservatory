d8a60307b85a85d5319c7b09d3b0e5dedb77d6f0
thesfromthed <thesfromthed@gmail.com>
2021-09-11 22:49:24 +0800

    teleport_20210911
    teleport_20210911
    Xueyuan Tang

---
+++ DREAMS Unity/DREAMS/Assets/Scripts/teleport/lightcontrol.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightcontrol : MonoBehaviour
+{
+    public GameObject currentLight;
+    public GameObject[] teleportlights = new GameObject[3];
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Cursor.visible = true;
+        foreach (GameObject obj in teleportlights)
+        {
+            if (obj)
+            {
+                obj.GetComponent<Renderer>().enabled = true;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Cursor.visible = false;
+
+        foreach (GameObject obj in teleportlights)
+        {
+            if (obj)
+            {
+                obj.GetComponent<Renderer>().enabled = false;
+            }
+        }
+    }
+}
