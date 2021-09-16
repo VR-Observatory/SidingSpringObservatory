8ce9ae54b6e8aaa0d1e948064578645583acfcb7
Jianan Wu <jianan.wu@anu.edu.au>
2021-09-16 20:44:53 +1000

    Update AAT building Demo

---
+++ DREAMS Unity/DREAMS/Assets/Scenes/DemoScene/PlayerDemo/PlayerSetting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSetting : MonoBehaviour
+{
+    [Header("Player Height")]
+    [Tooltip("The slider in menu canvas to change player's height")]
+    public Slider heightSlider;
+    public float heightMaxValue;
+    public float heightMinValue;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
