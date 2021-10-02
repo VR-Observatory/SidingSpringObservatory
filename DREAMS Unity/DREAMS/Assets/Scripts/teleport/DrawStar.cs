438123c650ccf1c04af0e95fa4a5114ff1ba3e34
thesfromthed <thesfromthed@gmail.com>
2021-10-02 18:46:00 +0800

    VR_PC_Click demo

--- DREAMS Unity/DREAMS/Assets/Scripts/teleport/DrawStar.cs
+++ DREAMS Unity/DREAMS/Assets/Scripts/teleport/DrawStar.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class DrawStar : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class DrawStar : MonoBehaviour
     public GameObject teleport_notice;
     public bool whether_first = false;
     private bool whether_VR = false;
+    private bool whether_colorchanged = false;
+    private GameObject last_hit;
     void Start()
     {
         if (OVRManager.isHmdPresent)
@@ -41,15 +44,51 @@ public class DrawStar : MonoBehaviour
             if (Physics.Raycast(ray, out hit, 1000, guideboard_layer))
             {
                 GUI.DrawTexture(rect, texture);
+                if (hit.collider.gameObject.GetComponent<click_teleport>() == null)
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        ColorBlock cb = hit.collider.gameObject.GetComponent<Button>().colors;
+                        hit.collider.gameObject.GetComponent<Image>().color = cb.pressedColor;
+                        whether_colorchanged = true;
+                        last_hit = hit.collider.gameObject;
+                        //hit.collider.gameObject.GetComponent<Button>().colors = cb;
+                    }
+                }
 
                 if (Input.GetMouseButtonUp(0))
                 {
-                    //cvs2.GetComponent<button_delete>().debugClick();
-                    hit.collider.gameObject.GetComponent<click_teleport>().OnClick();
+                    if (hit.collider.gameObject.GetComponent<click_teleport>() == null)
+                    {
+                        ColorBlock cb = hit.collider.gameObject.GetComponent<Button>().colors;
+                        hit.collider.gameObject.GetComponent<Image>().color = cb.normalColor;
+                        whether_colorchanged = false;
+
+                        //hit.collider.gameObject.GetComponent<Button>().colors = cb;
+                        //cvs2.GetComponent<button_delete>().debugClick();
+                        //hit.collider.gameObject.GetComponent<click_teleport>().OnClick();
+                        hit.collider.gameObject.GetComponent<Button>().onClick.Invoke();
+                    }
+                    else
+                    {
+                        hit.collider.gameObject.GetComponent<click_teleport>().OnClick();
+                    }
+
+                    
+                    //hit.collider.gameObject.GetComponent<Button>().Select();
                 }
             }
             else
             {
+
+                if (whether_colorchanged == true)
+                {
+                    ColorBlock cb = last_hit.GetComponent<Button>().colors;
+                    last_hit.GetComponent<Image>().color = cb.normalColor;
+                    whether_colorchanged = false;
+                }
+
+
                 Rect rect02 = new Rect(center_pos.x - (texture02.width / 2), Screen.height - center_pos.y - (texture02.height / 2), texture02.width, texture02.height);
                 GUI.DrawTexture(rect, texture02);
 
---
+++ DREAMS Unity/DREAMS/Assets/Scripts/teleport/UI_star.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_star : MonoBehaviour
+{
+    public GameObject Crosshairs;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        Crosshairs.GetComponent<DrawStar>().Crosshairs_visible = true;
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Crosshairs.GetComponent<DrawStar>().Crosshairs_visible = false;
+
+    }
+}
