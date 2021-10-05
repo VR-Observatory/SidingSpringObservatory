a80f3b28a81a322ff353651db47f46ee2bc55782
thesfromthed <thesfromthed@gmail.com>
2021-10-05 23:36:36 +0800

    guideboard update

--- DREAMS Unity/DREAMS/Assets/Scripts/teleport/resize_reverse.cs
+++ DREAMS Unity/DREAMS/Assets/Scripts/teleport/resize_reverse.cs
@@ -14,7 +14,7 @@ public class resize_reverse : MonoBehaviour
         child.GetComponent<Button>().onClick.AddListener(buttonclick);
         transform.localPosition = new Vector3((float)-0.51, 0, 0);
         transform.localScale = new Vector3(1, 1, 1);
-        transform.localEulerAngles = new Vector3(0, 180, 0);
+        transform.localEulerAngles = new Vector3(0, 270, 0);
         GetComponent<button_control>().disableButton();
     }
 
